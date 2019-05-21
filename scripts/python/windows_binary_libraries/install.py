414154c23f05498452cf0388e3ac0d8ec1774916
oryon-dominik <oryon@soila.de>
2019-05-21 09:42:16 +0200

    windows binaries auto_install

---
+++ scripts/python/windows_binary_libraries/install.py
@@ -0,0 +1,15 @@
+"""
+installs all python modules found inside this folder
+"""
+
+from pathlib import Path
+import subprocess
+import sys
+
+directory = Path('.')
+
+for obj in directory.iterdir():
+        if obj.is_file():
+            if str(obj) != __file__:
+                package = str(obj)
+                subprocess.call([sys.executable, "-m", "pip", "install", package])
