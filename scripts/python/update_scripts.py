15b172014f46ccf114ba91cf2bcad69ccde9049c
oryon-dominik <oryon@soila.de>
2019-05-21 13:57:58 +0200

    added script to update local scripts from repositories

---
+++ scripts/python/update_scripts.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+deploys new versions from the repos to the scripts
+"""
+
+from pathlib import Path
+import os
+import shutil
+
+clitube_new, clitube_old = Path(""), Path("")
+
+if os.getenv("DEN_ROOT"):
+    root = Path(os.getenv("DEN_ROOT"))
+    clitube_old = root / "scripts" / "python" / "cliTube.py"
+
+if os.getenv("PROJECTS_DIR"):
+    projects = Path(os.getenv("PROJECTS_DIR"))
+    clitube_new = projects / "cliTube" / "cliTube.py"
+
+if clitube_old.is_file() and clitube_new.is_file():
+    shutil.copy(clitube_new, clitube_old)
