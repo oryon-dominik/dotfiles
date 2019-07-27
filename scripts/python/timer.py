cb9ca812abe0172a2a1ab344e346ef4252969a47
oryon-dominik <oryon@soila.de>
2019-07-27 10:03:33 +0200

    updated timer-script

--- scripts/python/timer.py
+++ scripts/python/timer.py
@@ -3,17 +3,18 @@
 
 """
 Timer - for pomodore-sessions and meetings - for Windows
-plays "church.wav" from your preconfigured files-repo
+plays waves from your preconfigured files-repo
 """
 
 __version__ = "0.1.0"
 __author__ = "oryon/dominik"
 __date__ = "April 12, 2019"
-__updated__ = "May 21, 2019"
+__updated__ = "July 27, 2019"
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 import time
 import winsound
+import win32com.client as wincl
 from pathlib import Path
 import os
 
@@ -33,12 +34,12 @@ parser.add_argument(
     help="time in minutes till alarm clock",
     nargs=1,
 )
-parser.add_argument("soundfile", nargs="?", default="church")
+parser.add_argument("soundfile", nargs="?", default=None, help="soundfile to play from files/systemsounds")
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
-
+    filename = f"{args.soundfile}.wav" if args.soundfile else None
     timer = args.time[0]
 
     time_left = int(timer) * 60
@@ -51,13 +52,17 @@ if __name__ == "__main__":
         minutes, seconds = divmod(time_left, 60)
         print(f"Time left: - {minutes:02d}:{seconds:02d}", end="\n")
 
-    if os.getenv("DEN_ROOT"):
+    if os.getenv("DEN_ROOT") and filename:
         cfg_root = Path(os.getenv("DEN_ROOT"))
-        filename = f"{args.soundfile}.wav"
         soundfile = cfg_root / "files" / "systemsounds" / filename
+    elif filename:
+        cwd = Path(".")
+        soundfile = cwd / filename
     else:
         soundfile = Path("")
 
     if soundfile.is_file():
         winsound.PlaySound(str(soundfile), winsound.SND_FILENAME)
-
+    else:        
+        voice = wincl.Dispatch("SAPI.SpVoice")
+        voice.Speak("The timer has finished")
