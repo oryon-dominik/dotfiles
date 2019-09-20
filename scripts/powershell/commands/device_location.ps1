1839561ffa0b1fef753e0fba72a465631c086627
oryon-dominik <oryon@soila.de>
2019-09-20 08:51:40 +0200

    location checks online status

--- scripts/powershell/commands/device_location.ps1
+++ scripts/powershell/commands/device_location.ps1
@@ -12,33 +12,40 @@ $gps_location = New-Object System.Device.Location.GeoCoordinate
 $latitude = $gps_location.latitude
 $longitude = $gps_location.longitude
 
-if ([System.Double]::IsNaN($latitude)){
-    $latitude = $home_latitude
-}
-if ([System.Double]::IsNaN($longitude)){
-    $longitude = $home_longitude
-}
+$connected = Test-Connection -Count 1 -ComputerName google.de -Quiet -InformationAction Ignore
 
-if (-Not [System.Double]::IsNaN($latitude) -and -Not [System.Double]::IsNaN($longitude))
-    {
-        # Retrieve Geolocation from Google Geocoding API
-        $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$GEOCODING_KEY"
-
-        
-        # TODO: catch no network-error
-        $APIResults = ((Invoke-WebRequest $url).Content | ConvertFrom-Json | Select Results)
-        
-        if ($APIResults.results) {
-            $GEO_country = $APIResults.results[0].address_components[-2].short_name
-            $GEO_city = $APIResults.results[0].address_components[-4].long_name
-        }
-            
-        if(-Not $GEO_country -or $GEO_country -eq $null){ $GEO_country = $home_country }
-        if(-Not $GEO_city -or $GEO_city -eq $null){ $GEO_city = $home_city }
-
-        $position = New-Object -TypeName PSObject
-        $position | Add-Member -MemberType NoteProperty -Name city -Value $GEO_city
-        $position | Add-Member -MemberType NoteProperty -Name country -Value $GEO_country
-
+if ($connected) {
+    if ([System.Double]::IsNaN($latitude)){
+        $latitude = $home_latitude
     }
-Else { Write-Warning "Latitude or Longitude data missing" }
+    if ([System.Double]::IsNaN($longitude)){
+        $longitude = $home_longitude
+    }
+
+    if (-Not [System.Double]::IsNaN($latitude) -and -Not [System.Double]::IsNaN($longitude))
+        {
+            # Retrieve Geolocation from Google Geocoding API
+            $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$GEOCODING_KEY"
+
+            
+            # TODO: catch no network-error
+            $APIResults = ((Invoke-WebRequest $url).Content | ConvertFrom-Json | Select Results)
+            
+            if ($APIResults.results) {
+                $GEO_country = $APIResults.results[0].address_components[-2].short_name
+                $GEO_city = $APIResults.results[0].address_components[-4].long_name
+            }
+                
+            if(-Not $GEO_country -or $GEO_country -eq $null){ $GEO_country = $home_country }
+            if(-Not $GEO_city -or $GEO_city -eq $null){ $GEO_city = $home_city }
+
+            $position = New-Object -TypeName PSObject
+            $position | Add-Member -MemberType NoteProperty -Name city -Value $GEO_city
+            $position | Add-Member -MemberType NoteProperty -Name country -Value $GEO_country
+
+        }
+    Else { Write-Warning "Latitude or Longitude data missing" }
+}
+Else {
+    Write-Host "No Connection to GEOCODING-API possible"
+}
