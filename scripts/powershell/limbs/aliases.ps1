bdb56ce64d8daab23298e24e821f1443143a5937
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2020-08-07 10:10:26 +0200

    fix

--- scripts/powershell/limbs/aliases.ps1
+++ scripts/powershell/limbs/aliases.ps1
@@ -62,9 +62,9 @@ Set-Alias -Name np -Value notepad++ -Description "edit file fast"
 
 # docker
 function up {docker-compose up}
-function mm {docker-compose run --rm run django python manage.py makemigrations}
-function mig {docker-compose run --rm run django python manage.py migrate}
-function te {docker-compose run --rm run django pytest}
+function mm {docker-compose run --rm django python manage.py makemigrations}
+function mig {docker-compose run --rm django python manage.py migrate}
+function te {docker-compose run --rm django pytest}
 
 # loading local aliases last, to overwrite existing ones
 # load locations
