543dfe669ee4b6fe76ae4b73dc0400e6dca90442
oryon-dominik <oryon@cyberise.de>
2020-12-22 20:26:23 +0100

    wsl-display

--- ubuntu/home/.config/fish/functions/wsl_config.fish
+++ ubuntu/home/.config/fish/functions/wsl_config.fish
@@ -1,7 +1,7 @@
 function wsl_config
     # X-server to Windows
     set wsl_ip (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
-    set -U DISPLAY "$wsl_ip":10.0
+    set -U DISPLAY "$wsl_ip":0
     # openGL for the X-server
     set -U LIBGL_ALWAYS_INDIRECT 1
     # remove windows pyenv and poetry from path
