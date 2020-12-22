52ccce61f49bfdefae1529ce7f6e376380b3f4e8
oryon-dominik <oryon@cyberise.de>
2020-12-22 19:55:48 +0100

    add ssh-agent

--- ubuntu/home/.config/fish/config.fish
+++ ubuntu/home/.config/fish/config.fish
@@ -28,6 +28,15 @@ register-python-argcomplete --shell fish pipx | source
 
 set fish_color_cwd yellow
 
+# ssh-agent
+if not pgrep -f ssh-agent > /dev/null
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+
 if status is-interactive
   cd $HOME
 end
+
