959a9944bc5f5ad24958e541ecf0d9c68db4728a
oryon-dominik <oryon@cyberise.de>
2021-01-10 20:53:12 +0100

    more aliases

--- scripts/powershell/limbs/aliases.ps1
+++ scripts/powershell/limbs/aliases.ps1
@@ -10,11 +10,7 @@
 # load machine
 . $PSScriptRoot\..\machines\load_machine.ps1
 
-Set-Alias -Name ll -Value Get-ChildItem -Description "linux ll like"
-Set-Alias -Name la -Value Get-ChildItem -Force -Description "linux ll like"
-
-Set-Alias -Name draw -Value Write-Pixel -Description "Draws pictures in pixel into shell"
-
+# managing the computer's state
 Set-Alias -Name reboot -Value Restart-Computer -Description "reboot"
 Set-Alias -Name reb -Value Restart-Computer -Description "reboot"
 # %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState Hibernate
@@ -22,29 +18,29 @@ function hibernate {& "$env:windir\system32\shutdown.exe" /h}
 Set-Alias -Name hib -Value hibernate -Description "hibernation"
 Set-Alias -Name aus -Value Stop-Computer -Description "shutdown"
 
+# some linux aliases
+Set-Alias -Name ll -Value Get-ChildItem -Description "linux ll like"
+Set-Alias -Name la -Value Get-ChildItem -Force -Description "linux ll like"
 Set-Alias -Name touch -Value New-Item -Description "Creates a file"
-Set-Alias -Name whereis -Value Get-Command -Description "Shows commands locations"
-Set-Alias -Name manpage -Value Get-help -Description "Manpage"
 
 function linux_grep($string, $file) {sls $string .\$file -ca}
 Set-Alias -Name grep -Value linux_grep -Description "Linux like grep"
 
+Set-Alias -Name whereis -Value Get-Command -Description "Shows commands locations"
+Set-Alias -Name manpage -Value Get-help -Description "Manpage"
+Set-Alias -Name cat -Value lolcat -Description "Replace cat with colors :) from lolcat (installation required)"
+
+Set-Alias -Name draw -Value Write-Pixel -Description "Draws pictures in pixel into shell"
 function md5($filepath) {Get-FileHash $filepath -Algorithm MD5}
 
-# docker-compose
-Set-Alias -Name dc -Value docker-compose -Description "Docker-Compose"
 
 # development
-function webserver { python "-m http.server 80" }
-
 Set-Alias git hub  # integrates hub into git command-line (https://github.com/github/hub)
+function webserver { python "-m http.server 80" }
 
 # media
 $env:path += ";C:\Program Files\VideoLAN\VLC\vlc.exe"
 Set-Alias -Name play -Value vlc -Description "Plays media with vlc"
-
-Set-Alias -Name clitube -Value (Join-Path -Path $script_location -ChildPath "\python\cliTube.py") -Description "Plays Youtube Search-Results"  # needs cliTube.py in $script_location
-Set-Alias -Name timer -Value (Join-Path -Path $script_location -ChildPath "\python\timer.py") -Description "Starts a timer"  # needs timer.py in $script_location
 
 # programs
 function jup {jupyter notebook}
@@ -61,12 +57,17 @@ Set-Alias -Name pdf -Value SumatraPDF -Description "reads pdf"
 Set-Alias -Name np -Value notepad++ -Description "edit file fast"
 
 # docker
+Set-Alias -Name dc -Value docker-compose -Description "Docker-Compose"
 function up {docker-compose up}
 function build {docker-compose build}
 function mm {docker-compose run --rm django python manage.py makemigrations}
 function mig {docker-compose run --rm django python manage.py migrate}
 function te {docker-compose run --rm django pytest}
 function init {docker-compose run --rm django python manage.py initialize; docker-compose run --rm django python manage.py createusers}
+
+# custom-dotfiles-scripts
+Set-Alias -Name clitube -Value (Join-Path -Path $script_location -ChildPath "\python\cliTube.py") -Description "Plays Youtube Search-Results"  # needs cliTube.py in $script_location
+Set-Alias -Name timer -Value (Join-Path -Path $script_location -ChildPath "\python\timer.py") -Description "Starts a timer"  # needs timer.py in $script_location
 
 # loading local aliases last, to overwrite existing ones
 # load locations
