f8a7526edc530679e05ba01e48691e22a1b9543d
oryon-dominik <oryon@cyberise.de>
2021-04-15 11:56:15 +0200

    update_script is still missing validation and error handling

--- scripts/python/update_scripts.py
+++ scripts/python/update_scripts.py
@@ -8,6 +8,7 @@ deploys new versions from the repos to the scripts
 from pathlib import Path
 import os
 import shutil
+import subprocess
 
 clitube_new, clitube_old = Path(""), Path("")
 
@@ -22,6 +23,12 @@ if os.getenv("PROJECTS_DIR"):
 
 if clitube_old.is_file() and clitube_new.is_file():
     shutil.copy(clitube_new, clitube_old)
+    # TODO: build binary, and/or copy binary only
+    # subprocess.run(["clitube", "--update"])
+    # python -m pip install pyinstaller
+    # pyinstaller.exe --onefile cliTube.py --distpath . --clean
+    # have to clean up the mess manually..
+    # rm clitube.spec; rm build
+    # clitube --update
 
-# TODO: add the new youtube.lua to the scripts update..
 # TODO: add logic (maybe in ps-scripts) to update periodically
