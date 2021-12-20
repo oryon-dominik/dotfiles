04526b3b3eeed29b33bc7e86ba6d19644042c839
oryon-dominik <oryon@cyberise.de>
2021-12-20 19:27:55 +0100

    modern unix error-handling

--- install/windows/modern_unix_for_windows.ps1
+++ install/windows/modern_unix_for_windows.ps1
@@ -1,76 +1,94 @@
-
-# TODO: check git + cargo + go + npm on path
+#!/usr/bin/env pwsh
 
 Write-Host "Installing modern unix cli-commands to powershell..."
 
-# Rust modules
+if (Test-Path "cargo") {
+    # Rust modules
+    # (btm) graphical system/process monitor - https://github.com/ClementTsang/bottom
+    cargo install bottom
 
-# (btm) graphical system/process monitor - https://github.com/ClementTsang/bottom
-cargo install bottom
+    # Replacement for ps - https://github.com/dalance/procs
+    cargo install procs
 
-# Replacement for ps - https://github.com/dalance/procs
-cargo install procs
+    # instant overview of which directories are using disk - https://github.com/bootandy/dust
+    cargo install du-dust
 
-# instant overview of which directories are using disk - https://github.com/bootandy/dust
-cargo install du-dust
+    # CLI benchmarks - https://github.com/sharkdp/hyperfine
+    cargo install hyperfine
 
-# CLI benchmarks - https://github.com/sharkdp/hyperfine
-cargo install hyperfine
+    # send HTTP requests - https://github.com/ducaale/xh
+    cargo install xh
 
-# send HTTP requests - https://github.com/ducaale/xh
-cargo install xh
+    # grahpical directory trees - https://github.com/Canop/broot
+    cargo install broot
 
-# grahpical directory trees - https://github.com/Canop/broot
-cargo install broot
+    # (tldr) Help pages for command-line tools, rust implementation of tldr - https://github.com/dbrgn/tealdeer
+    cargo install tealdeer
 
-# (tldr) Help pages for command-line tools, rust implementation of tldr - https://github.com/dbrgn/tealdeer
-cargo install tealdeer
+    # build regexes from CLI-tests https://github.com/pemistahl/grex
+    cargo install grex
 
-# build regexes from CLI-tests https://github.com/pemistahl/grex
-cargo install grex
+    # current network utilization - https://github.com/imsnif/bandwhich
+    cargo install bandwhich
 
-# current network utilization - https://github.com/imsnif/bandwhich
-cargo install bandwhich
+    # statistics about your code - https://github.com/XAMPPRocky/tokei
+    cargo install tokei
 
-# statistics about your code - https://github.com/XAMPPRocky/tokei
-cargo install tokei
+    # field selection from content - https://github.com/theryangeary/choose
+    cargo install choose
 
-# field selection from content - https://github.com/theryangeary/choose
-cargo install choose
+    if (Test-Path "git") {
+        # create the (gitignored) .dotfiles/bin directory if it doesn't exist
+        mkdir $env:DOTFILES/bin -ErrorAction SilentlyContinue
 
-# create the 
-mkdir $env:DOTFILES/bin -ErrorAction SilentlyContinue
+        # A modern replacement for ls - https://github.com/ogham/exa
+        # cargo install exa
+        git clone https://github.com/ogham/exa $env:DOTFILES/bin/exa
+        cd $env:DOTFILES/bin/exa
+        # cherry pick the windows fix
+        git fetch origin pull/820/head:chesterliu/dev/win-support
+        git checkout chesterliu/dev/win-support
+        cargo install --path . --force
+        cd -
 
-# command-line DNS client - https://github.com/ogham/dog
-# cargo install dog
+        # command-line DNS client - https://github.com/ogham/dog
+        # cargo install dog
+        git clone https://github.com/ogham/dog $env:DOTFILES/bin/dog
+        cd $env:DOTFILES/bin/dog
+        cargo install --path . --force
+        cd -
 
-# git clone https://github.com/ogham/dog $env:DOTFILES/bin/dog
-# cd $env:DOTFILES/bin/dog
-# cargo install --path . --force
-# cd -
+    } else {
+        Write-Host "git not found on PATH... skipping exa & dog"
+    }
+    
 
-git clone https://github.com/ogham/exa $env:DOTFILES/bin/exa
-cd $env:DOTFILES/bin/exa
-git fetch origin pull/820/head:chesterliu/dev/win-support
-git checkout chesterliu/dev/win-support
-cargo install --path . --force
-cd -
+} else {
+    Write-Host "cargo not found on PATH... skipping rust modules"
+}
 
+if (Test-Path "go") {
+    ## cheat
+    go install github.com/cheat/cheat/cmd/cheat@latest
+} else {
+    Write-Host "go not found on PATH... skipping cheat"
+}
+
+if (Test-Path "npm") {
+    # gtop (process/system visualization)
+    npm install gtop -g
+} else {
+    Write-Host "node not found on PATH... skipping gtop"
+}
 
 Write-Host "Pick the latest windows executable for these additional packages from github."
 Write-Host "Save them to $(Join-Path -Path $env:USERPROFILE -ChildPath '\.cargo\bin\')."
 Write-Host ""
-Write-Host "dog - dns lookup"
-Write-Host "https://github.com/ogham/dog/releases"
+# Write-Host "dog - dns lookup"
+# Write-Host "https://github.com/ogham/dog/releases"
 Write-Host ""
 Write-Host "jq - Command-line JSON processor"
 Write-Host "https://github.com/stedolan/jq/releases"
 Write-Host ""
 Write-Host "curlie - The power of curl, the ease of use of httpie."
 Write-Host "https://github.com/rs/curlie/releases"
-
-## cheat
-go install github.com/cheat/cheat/cmd/cheat@latest
-
-# gtop (process/system visualization)
-npm install gtop -g
