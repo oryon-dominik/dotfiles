f9adec5f3aa3525e679c83b708764735cae679c7
oryon-dominik <oryon@cyberise.de>
2021-12-21 08:31:42 +0100

    refactored speedtest

--- scripts/powershell/commands/Speedtest.ps1
+++ scripts/powershell/commands/Speedtest.ps1
@@ -1,39 +1,24 @@
+# Connection Speedtest using the python module speedtest-cli
 
 # is python installed?
-$python_on_path_error_message = (&{python -V}).Exception.Message
-if ($python_on_path_error_message -ne $null) {
-    Write-Host "Could not find a python version Installed."
-    Write-Host $python_on_path_error_message
-    return
-}
-# is pyenv used? (we're manually buílding the python-path here)
-if (-not (Test-Path env:PYENV_HOME)) {
-    Write-Host "No PYENV_HOME environment variable set."
-    Write-Host "Python versions outside of pyenv are not supported yet."
-    Write-Host "Feel free to file a pull-request though :)."
-    return
+$python_path = whereis python | select -ExpandProperty Source
+
+if ((($python_path).Exception -ne $null) -And ($python_path).Exception.Message.StartsWith("Get-Command: The term '")) {  # is not recognized
+    Write-Host "Python is not installed. Please install python and try again."
+    exit
 }
 
-# get the correct path from active python
-$python_version = (&{python -V})
-$_, $version = $python_version -split ' '
-$version_path = Join-Path $env:PYENV_HOME -ChildPath "versions" | Join-Path -ChildPath $version
-
-# is the speedtest-cli available?
-$speedtest_module_path = Join-Path $version_path -ChildPath "Lib\site-packages\speedtest.py"
-if (-not (Test-Path $speedtest_module_path)) {
-    Write-Host "Python Module for Speedtest not found"
-    Write-Host "Install with: 'pip install speedtest-cli'"
-    return
+$pip_path = whereis pip | select -ExpandProperty Source
+if ((Split-Path -Path $python_path) -ne (Split-Path -Path $pip_path)) {
+    Write-Host "Python and pip paths don't match. Please fix your paths and try again."
+    exit
 }
 
-# is the executable available in python's script path
-$speedtest_script_path = Join-Path $version_path -ChildPath "Scripts\speedtest.exe"
-if (-not (Test-Path $speedtest_script_path)) {
-    Write-Host "Python Script for Speedtest not found"
-    Write-Host "Install with: 'pip install speedtest-cli'"
-    return
+$speedtest_Version = pip list | grep speedtest-cli
+if ($speedtest_Version -eq "") {
+    Write-Host "speedtest-cli is not installed. Please 'pip install speedtest-cli' and try again."
+    exit
 }
 
 # run the speedtest
-& $speedtest_script_path
+& python -W ignore::DeprecationWarning -m speedtest 
