ed15954852783b03f32ffac90a6a76539ba4d599
oryon-dominik <oryon@cyberise.de>
2021-12-29 10:20:16 +0100

    bugfix: ls should support args :)

--- scripts/powershell/acronyms/aliases.ps1
+++ scripts/powershell/acronyms/aliases.ps1
@@ -18,11 +18,11 @@ Set-Alias -Name aus -Value Stop-Computer -Description "shutdown"
 
 # ls -> exa
 Remove-Alias -Name ls
-function ls {exa --group-directories-first --git-ignore}
-function ll {exa --color-scale --long --header --group-directories-first}
-function la {exa --all --color-scale --long --header --group-directories-first}
-function lt {exa --tree --color-scale --group-directories-first}
-function l {exa --all --color-scale --long --header --git --group-directories-first}
+function ls {exa --group-directories-first --git-ignore $args}
+function ll {exa --color-scale --long --header --group-directories-first $args}
+function la {exa --all --color-scale --long --header --group-directories-first $args}
+function lt {exa --tree --color-scale --group-directories-first $args}
+function l {exa --all --color-scale --long --header --git --group-directories-first $args}
 
 # linux-like
 Set-Alias -Name touch -Value New-Item -Description "Creates a file"
