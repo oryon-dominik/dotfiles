9c28931dca67b47d094b7b267b4f9924f6c4c5c0
oryon-dominik <oryon@cyberise.de>
2021-12-30 09:03:56 +0100

    backup fix

--- common/powershell/components/Backups.ps1
+++ common/powershell/components/Backups.ps1
@@ -1,4 +1,6 @@
 
+# TODO: setup backups on new machine function, to set envs (or .env) - replace hardcoded paths
+
 # restic backups
 function restic-apollon {
     $is_elevated = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
@@ -34,7 +36,7 @@ function backups {
         Write-Host starting backup at (Get-Date)
         Write-Host --------------------------------------------
 
-        restic-apollon --tag apollon --exclude-file $env:DOTFILES\local\restic-excludefile backup c:\dev d:\keep
+        restic-apollon --tag apollon --exclude-file $env:DOTFILES\shared\backup\restic-excludefile backup c:\dev d:\keep
         restic-apollon forget --prune --keep-last=7 --keep-daily=30 --keep-weekly=52 --keep-monthly=24 --keep-yearly=10
     }
 }
