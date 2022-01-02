53ba9f8b22292f96d524f3d03bef11d38dc2ff00
oryon-dominik <oryon@cyberise.de>
2022-01-02 20:54:15 +0100

    ubuntu packages update

--- install/ubuntu/cargo_packages.sh
+++ install/ubuntu/cargo_packages.sh
@@ -1,2 +1,26 @@
+#!/bin/sh
 
-cargo install zoxide bottom procs du-dust hyperfine xh broot tealdeer grex bandwhich tokei choose exa dog mcfly
+# TODO: generate from https://github.com/oryon-dominik/ansible-ubuntu-desktop/blob/trunk/roles/rust/vars/main/crates.yml
+cargo install \
+ exa         \  # A modern replacement for ls - https://github.com/ogham/exa
+ bat         \  # cat clone with syntax highlighting and Git integration - https://github.com/sharkdp/bat
+ git-delta   \  # highlighting for diff - https://github.com/dandavison/delta
+ mcfly       \  # Fly through your shell history - https://github.com/cantino/mcfly
+ zoxide      \  # smarter cd command - https://github.com/ajeetdsouza/zoxide
+ procs       \  # Replacement for ps - https://github.com/dalance/procs
+ bottom      \  # (btm) graphical system/process monitor - https://github.com/ClementTsang/bottom
+ broot       \  # grahpical directory trees - https://github.com/Canop/broot
+ du-dust     \  # instant overview of which directories are using disk - https://github.com/bootandy/dust
+ ripgrep     \  # (rg) grep on steroids - https://github.com/BurntSushi/ripgrep
+ fd-find     \  # simple, fast and user-friendly alternative to 'find' - https://github.com/sharkdp/fd
+ hyperfine   \  # CLI benchmarks - https://github.com/sharkdp/hyperfine
+ sd          \  # find & replace - https://github.com/chmln/sd
+ choose      \  # field selection from content - https://github.com/theryangeary/choose
+ grex        \  # build regexes from CLI-tests https://github.com/pemistahl/grex
+ tokei       \  # statistics about your code - https://github.com/XAMPPRocky/tokei
+ gping       \  # Ping, but with a graph - https://github.com/orf/gping
+ dog         \  # command-line DNS client - https://github.com/ogham/dog
+ xh          \  # send HTTP requests - https://github.com/ducaale/xh
+ bandwhich   \  # current network utilization - https://github.com/imsnif/bandwhich
+ tealdeer    \  # (tldr) Help pages for command-line tools, rust implementation of tldr - https://github.com/dbrgn/tealdeer
+ alacritty      # Terminal emulator. - https://github.com/alacritty/alacritty
--- install/ubuntu/packages.sh
+++
@@ -1,5 +0,0 @@
-
-
-# -- on ubuntu we want to link batcat to bat
-# mkdir -p ~/.local/bin
-# ln -s /usr/bin/batcat ~/.local/bin/bat
