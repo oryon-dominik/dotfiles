858370585c6eeafa4b42a5b14bca1175bfc462a0
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2022-02-01 14:30:20 +0100

    hashlib: default is 656_ooo rounds now

--- scripts/powershell/PythonPowershell.ps1
+++ scripts/powershell/PythonPowershell.ps1
@@ -21,7 +21,7 @@ function cc () {
 
 
 function HashFromPassword {
-    python -c "from passlib.hash import sha512_crypt; import getpass; print(sha512_crypt.using(rounds=5000).hash(getpass.getpass()))"
+    python -c "from passlib.hash import sha512_crypt; import getpass; print(sha512_crypt.using(rounds=656_000).hash(getpass.getpass()))"
 }
 
 
