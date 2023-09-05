4ef4ce18d5e44d5e81615363a5d1148900f0af9b
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2023-09-05 11:35:17 +0200

    scoop buckets

---
+++ install/windows/InstallScoopBucktsAndPackages.ps1
@@ -0,0 +1,9 @@
+
+scoop bucket add extras
+scoop install vscode
+
+scoop bucket add nerd-fonts
+scoop install Cascadia-Code
+
+scoop bucket add doppler https://github.com/DopplerHQ/scoop-doppler.git
+scoop install doppler
