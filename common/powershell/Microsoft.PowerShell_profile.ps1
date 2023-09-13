d959c9c49624d64d331a833aae6110ce7dfcee25
oryon-dominik <oryon@cyberise.de>
2023-09-13 16:29:14 +0200

    version update

--- common/powershell/Microsoft.PowerShell_profile.ps1
+++ common/powershell/Microsoft.PowerShell_profile.ps1
@@ -69,6 +69,7 @@ $env:WORKON_HOME = "$env:USERPROFILE\venvs"
 . "$PSScriptRoot\components\GitPullHelpers.ps1"
 
 # add custom paths
+# TODO: read paths from a different path file at a different location and add it here successively
 $env:path += ";$Env:Programfiles\VideoLAN\VLC\vlc.exe"
 $env:path += ";$Env:Programfiles\NASM"  # netwide-assembler
 $env:path += ";$Env:Programfiles\GTK3-Runtime Win64\bin"  # gtk3 (used for weasyprint)
@@ -77,6 +78,7 @@ $env:path += ";$(Join-Path -Path "$script_location" -ChildPath "\batch")"
 $env:path += ";$(Join-Path -Path "$env:USERPROFILE" -ChildPath "\.cargo\bin\")"  # rust commands
 $env:path += ";$(Join-Path -Path "$env:PYENV_HOME" -ChildPath "\versions\$env:GLOBAL_PYTHON_VERSION\scripts\")"  # python scripts
 $env:path += ";$(Join-Path -Path "$env:USERPROFILE" -ChildPath "\go\bin\")"  # go commands
+$env:path += ";$(Join-Path -Path "$env:POETRY_HOME" -ChildPath "\bin")"  # python poetry
 # $env:path += ";$(Join-Path -Path "$env:USERPROFILE" -ChildPath "\AppData\Roaming\npm\")"  # npm
 if ($env:DOTFILES_SKIP_YARN -eq $false) {
   $env:path += ";$(yarn global bin)"
