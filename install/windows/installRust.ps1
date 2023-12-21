ac35f9cd92fe5615b142cc4e9cda82d40f9cedb5
oryon-dominik <oryon@cyberise.de>
2023-12-21 09:21:11 +0100

    install rust

--- install/windows/installRust.ps1
+++ install/windows/installRust.ps1
@@ -1,11 +1,13 @@
+Write-Host "Installing Rust Toolchain... (rustup, cargo, cargo-edit, cargo-expand.) https://www.rust-lang.org/"
 
-
-# TODO: Windows installations have to manually get
+# Windows installations could install manually
 # https://win.rustup.rs/x86_64
 # other installations have to rustup.rs and install 
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-Write-Error "Not Implemented (yet)"
 
+# Install using scoop:
+scoop install main/rustup
+scoop update rustup
 
 # ensure latest rust/cargo
 rustup update
@@ -13,7 +15,7 @@ rustup update
 # additional cargo tools and commands
 
 cargo install cargo-edit
-# or use the vendored version
-cargo install cargo-edit --features vendored-openssl
+# or use the vendored version to provide openssl out of the box
+# cargo install cargo-edit --features vendored-openssl
 
 cargo install cargo-expand
