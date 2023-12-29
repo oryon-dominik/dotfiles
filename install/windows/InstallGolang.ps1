484b161dfc77c57a82f004b0ebe93df5015f108a
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2023-12-29 09:50:22 +0100

    setting GOPATH as well.. (to better support scoop)

--- install/windows/InstallGolang.ps1
+++ install/windows/InstallGolang.ps1
@@ -3,8 +3,19 @@
 
 function InstallGolangToolchain {
 
+    param (
+        [string]$gopath = $($env:GOPATH)
+    )
+
+    if ($gopath -eq $null) {
+        $gopath = "$(Join-Path -Path $env:SCOOP -ChildPath 'apps\go\current\')"
+        Write-Host "No 'env:GOPATH' path given. Using default: '$gopath'."
+    }
+
     $installed = @()
-    
+
+    AddToDotenv -path "$env:DOTFILES\.env" -key "GOPATH" -value "$gopath" -overwrite $false -warn $false
+
     Write-Host "Installing Golang Toolchain... https://go.dev/"
 
     # Alternative: manually pick releases from https://go.dev/dl/.
