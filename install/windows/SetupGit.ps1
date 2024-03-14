b97b05d91078ed24699941f856ac8192b49be330
oryon-dominik <oryon@cyberise.de>
2024-03-14 14:03:25 +0100

    here-string-header

--- install/windows/SetupGit.ps1
+++ install/windows/SetupGit.ps1
@@ -63,7 +63,8 @@ function SetupLocalGitconfig {
 
 
     $localGitconfig = Get-Content $localGitconfigPath
-    $localGitconfig += @"[user]
+    $localGitconfig += @"
+[user]
     email = $email
     name = $name
 "@
